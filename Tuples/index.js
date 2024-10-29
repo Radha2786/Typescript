322c40f276f21a26cb07e9d444beb43d283fd52c
Radha Sharma <radhasharma2786@gmail.com>
2024-10-29 18:40:04 +0530

    diff b/w tuple and array

---
+++ Tuples/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// Array  
+const person1 = ["radha", 21, true];
+const person2 = [21, "boomer", true];
+const person3 = ["yash", 22, true];
+const person4 = ["Harshita", 22, true];
+console.log(person3[0]);
---
+++ Tuples/index.ts
@@ -0,0 +1,21 @@
+
+// Array  
+
+const person1 : any[] = ["radha", 21, true];
+const person2 : any[] = [21,"boomer", true];
+
+// console.log(person1[0]);
+// as we can see there is no specific type or order in the above array 
+
+// --------------------------Tuple Use Case ---------------------------- 
+// if we know about the specific order and size of the array we can use tuples
+
+// Tuple 
+
+type Person = [string, number, boolean];
+
+const person3 : Person = ["yash", 22, true];
+const person4 : Person = ["Harshita", 22, true];
+// const person5 : Person = [23,"Akash", false];   // This will give error as the order is not correct
+
+console.log(person3[0]);
